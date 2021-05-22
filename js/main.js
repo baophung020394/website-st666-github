c4de2f2d7c67ff15baa1a847f2794a587006ff9b
unknown <bao.future94@gmail.com>
2021-05-22 10:49:28 +0700

    fixed

--- js/main.js
+++ js/main.js
@@ -2,24 +2,69 @@ $(document).ready(() => {
   $(".carousel-home").owlCarousel({
     loop: true,
     responsiveClass: true,
-    nav: true,
-    // autoplayTimeout: 5000,
-    // autoplay: true,
-    navText: ["<img src='../img/item/left.jpg'>","<img src='../img/item/right.jpg'>"],
     responsive: {
       0: {
         items: 1,
-        nav:true,
+        dots: true,
+        nav: false,
       },
       768: {
         items: 1,
-        nav:true,
+        dots: false,
+        nav: true,
+        navText: [
+          "<img src='../img/item/left.jpg'>",
+          "<img src='../img/item/right.jpg'>",
+        ],
       },
       1024: {
         items: 1,
-        nav:true,
+        dots: false,
+        nav: true,
+        navText: [
+          "<img src='../img/item/left.jpg'>",
+          "<img src='../img/item/right.jpg'>",
+        ],
+      },
+    },
+  });
+  $(".carousel-mobile").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    dots: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1024: {
+        items: 1,
         margin: 0,
       },
     },
+  });
+  //
+  $(".menu-item-mobile a").on("click", function () {
+    $(this).find('.actions').toggleClass('active');
+    $('body').find('.bg-overlay').css({ 'display': 'block'});
+  }); 
+  $(".bg-overlay").on("click", function () {
+    $(this).css({ 'display': 'none'});
+    $('body').find('.actions').removeClass('active');
+  }); 
+  $(".open-menu").on("click", function () {
+    $('body').find('.menu-mobile').addClass('active');
+    $('body').find('.bg-overlay-black').css({ 'display': 'block'});
+  }); 
+  $(".btn-closes").on("click", function () {
+    $('body').find('.menu-mobile').removeClass('active');
+    $('body').find('.bg-overlay-black').css({ 'display': 'none'});
+  }); 
+  $(".bg-overlay-black").on("click", function () {
+    $(this).css({ 'display': 'none'});
+    $('body').find('.menu-mobile').removeClass('active');
   });
 });
